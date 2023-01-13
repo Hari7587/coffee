9916ed907fddd8f19a7c56d45c462b02156a6019
hari7587 <harish.h.blr87@gmail.com>
2023-01-13 19:09:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://media.giphy.com/media/b0K8vtlBheVn0YDIbf/giphy-downsized-large.gif" alt="logo" />
+        <h2>I Love Coffee..!</h2>        
+      </header>
+    </div>
+  );
+}
+
+export default App;

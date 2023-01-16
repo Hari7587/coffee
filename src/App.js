f9c151daa169568617ba2ff82f24037a40ac99fe
hari7587 <harish.h.blr87@gmail.com>
2023-01-16 16:25:55 +0530

    Changed the text again to coffee

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="https://media.giphy.com/media/b0K8vtlBheVn0YDIbf/giphy-downsized-large.gif" alt="logo" />
-        <h2>I Love Coffee..!</h2>        
+        <h2>I love Coffee..!</h2>        
       </header>
     </div>
   );
